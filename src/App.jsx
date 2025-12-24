d95ac39e4f2fe24cb6e1afd362c97069e08eb68b
Abhimanyu <dubabhimanyu@gmail.com>
2025-12-24 15:49:11 +0530

    Renaming folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 import Cart from "./pages/Cart/Cart";
 import Footer from "./components/Footer/Footer";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
